61c64d440372a1c73d9abeaddc8dc549af9879f2
Breno Paim Targa <brenotarga@gmail.com>
2025-03-12 14:54:19 -0300

    Resolved merge conflicts in algos.py and create-random-data.py

--- create-random-data.py
+++ create-random-data.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 import time
 import json
@@ -19,25 +18,3 @@ def createDatabase(size, maxNumber):
 
 for size in range(5_000, 50_001, 5_000):
     createDatabase(size, 1_000_000)
-=======
-import random
-import time
-import json
-
-def createDatabase(size, maxNumber):
-    t1 = time.perf_counter()
-    filename = f'db{size}params.json' 
-    numbers = random.choices(range(1, maxNumber + 1), k=size)
-
-    try:
-        with open(filename, "w") as f:
-            json.dump(numbers, f)
-    except IOError as e:
-        print(f"File error: {e}")
-
-    t2 = time.perf_counter()
-    print(f'Generated {filename} in {t2 - t1:.4f} seconds')
-
-for size in range(5000, 50001, 5000):
-    createDatabase(size, 1000000)
->>>>>>> 4de165c3a4ccd887efdd3a8fb44259870671f769
